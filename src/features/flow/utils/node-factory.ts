698b64f6cedcb0a92222ef8de52c99c8b7e85bfa
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2025-09-22 17:29:11 +0900

    [feat] 아이디 생성방식 변경

--- src/features/flow/utils/node-factory.ts
+++ src/features/flow/utils/node-factory.ts
@@ -1,12 +1,18 @@
 /**
  * 노드 생성과 관련된 헬퍼 함수 모음.
  */
+import { generateSlug } from "random-word-slugs";
 import { nodeTypeConfigs } from "@/features/flow/constants/node-config";
 import type { FlowNodeType, NodeData } from "@/features/flow/types/nodes";
 
-let sequence = 0;
+export const getId = () => {
+  const randomName = generateSlug(2, {
+    format: "kebab",
+    partsOfSpeech: ["adjective", "noun"],
+  }).replace(/-/g, "_"); // 'sleepy-fox' -> 'sleepy_fox'
 
-export const getId = () => `dndnode_${sequence++}`;
+  return `dndnode_${randomName}`; // ex: dndnode_sleepy_fox
+};
 
 export const createNodeData = (type: FlowNodeType): NodeData => {
   const config = nodeTypeConfigs[type];
